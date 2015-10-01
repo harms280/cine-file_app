1c0e2952e62a4f636bbe8c3b560ff8bec690a09f
aaron <harms280@gmail.com>
2015-10-01 10:11:26 -0700

    changed mongo connect

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cinefile_app');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/cinefile_app');
 
 mongoose.set('debug', true);
 
